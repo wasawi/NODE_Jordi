487d2d670beedaacab829ab2083116a469f232c6
wasawi <criccrec@gmail.com>
2015-09-22 20:03:57 +0200

    introducido funcion

--- M5_S2_2_collaborative_project/src/ofApp.cpp
+++ M5_S2_2_collaborative_project/src/ofApp.cpp
@@ -25,22 +25,55 @@ void ofApp::update(){
 void ofApp::draw(){
 
 	// Draw Jordi's color
-	ofSetColor(colorJordi);
-	ofRect(100, 100, 100, 100);
-	ofDrawBitmapStringHighlight("Jordi's color", 100,200);
+//	ofSetColor(colorJordi);
+//	ofRect(100, 100, 100, 100);
+//	ofDrawBitmapStringHighlight("Jordi's color", 100,200);
+
+	dibujaRecuadro(colorJordi, ofPoint(100,100), 100, 100, "Jordi's color");
 	
 	// Draw Alvaro's color
-	ofSetColor(colorAlvaro);
-	ofRect(200, 200, 100, 100);
-	ofDrawBitmapStringHighlight("Alvaro's color", 200,300);
+//	ofSetColor(colorAlvaro);
+//	ofRect(200, 200, 100, 100);
+//	ofDrawBitmapStringHighlight("Alvaro's color", 200,300);
 
+	dibujaRecuadro(colorAlvaro, ofPoint(200,200), 100, 100, "Alvaro's color");
+	
 	// Draw Fernando's color
-	ofSetColor(colorFernando);
-	ofRect(400, 400, 100, 100);
-	ofDrawBitmapStringHighlight("Fernando's color", 400,500);
+//	ofSetColor(colorFernando);
+//	ofRect(400, 400, 100, 100);
+//	ofDrawBitmapStringHighlight("Fernando's color", 400,500);
 
+	dibujaRecuadro(colorFernando, ofPoint(400,400), 100, 100, "Fernando's color");
+	
 	// Draw JuanCarlos’s color
-	ofSetColor(colorJuanCarlos);
-	ofRect(100, 400, 100, 100);
-	ofDrawBitmapStringHighlight("JuanCarlos color", 100,500);
+//	ofSetColor(colorJuanCarlos);
+//	ofRect(100, 400, 100, 100);
+//	ofDrawBitmapStringHighlight("JuanCarlos color", 100,500);
+	
+	dibujaRecuadro(colorJuanCarlos, ofPoint(100,400), 100, 100, "JuanCarlos color");
+	
 }
+
+
+//--------------------------------------------------------------
+void ofApp::dibujaRecuadro(ofColor myColor, ofPoint pos, int w, int h, string texto){
+	
+	
+	ofSetColor(myColor);
+	ofRect(pos, w, h);
+	//ofDrawBitmapStringHighlight(texto, 100,200);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- M5_S2_2_collaborative_project/src/ofApp.h
+++ M5_S2_2_collaborative_project/src/ofApp.h
@@ -7,6 +7,9 @@ class ofApp : public ofBaseApp{
 		void setup();
 		void update();
 		void draw();
+
+		void dibujaRecuadro(ofColor myColor, ofPoint pos, int w, int h, string texto);
+	
 	
 	ofColor colorJordi;
 	ofColor colorAlvaro;
