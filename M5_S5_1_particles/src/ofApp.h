622ae5e4d158782235e2a9e8f8739695bbe63559
wasawi <criccrec@gmail.com>
2015-10-12 23:06:22 +0200

    duplicate collaborative project into particles

---
+++ M5_S5_1_particles/src/ofApp.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "ofMain.h"
+#include "recuadro.h"
+
+class ofApp : public ofBaseApp{
+	public:
+		void setup();
+		void update();
+		void draw();
+
+	recuadro r;
+
+	ofColor colorJordi;
+	ofColor colorAlvaro;
+	ofColor colorFernando;
+	ofColor colorJuanCarlos;
+	ofColor colorSergio;
+};
